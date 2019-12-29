37671ae4245c445c93d00aeb7bb6c5b2ef7a6416
Starkfans <gengbin29080@hundsun.com>
2019-12-29 15:52:35 +0800

    搜索结果页面添加搜索功能

--- public/m/js/searchList.js
+++ public/m/js/searchList.js
@@ -15,7 +15,23 @@ $(function () {
     },function (data) {
         /*渲染数据*/
         $('.ct_product').html(template('list',data));
-    })
+    });
+    /*3.用户输入新的关键词，点击搜索，重置排序功能*/
+    $('.ct_search a').on('tap',function () {
+        var key = $.trim($input.val());
+        if(!key){
+            mui.toast('请输入关键字');
+            return false;
+        }
+        getSearchData({
+            proName: key,
+            page:1,
+            pageSize: 4
+        },function (data) {
+            $('.ct_product').html(template('list',data));
+        });
+    });
+
 });
 
 var getSearchData = function (params, callback) {
