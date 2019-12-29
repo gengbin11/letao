d3457c7095f5136dc1becde3928f333e53677be0
Starkfans <gengbin29080@hundsun.com>
2019-12-29 19:47:10 +0800

    商品搜索结果根据排序条件变化，排序功能完成

--- public/m/js/searchList.js
+++ public/m/js/searchList.js
@@ -9,46 +9,64 @@ $(function () {
     var $input = $('input').val(urlParams.key || '')
     /*2.页面初始化的时候，根据关键字查询第一页数据，4条*/
     getSearchData({
-        proName:urlParams.key,
-        page:1,
-        pageSize:4
-    },function (data) {
+        proName: urlParams.key,
+        page: 1,
+        pageSize: 4
+    }, function (data) {
         /*渲染数据*/
-        $('.ct_product').html(template('list',data));
+        $('.ct_product').html(template('list', data));
     });
     /*3.用户输入新的关键词，点击搜索，重置排序功能*/
-    $('.ct_search a').on('tap',function () {
+    $('.ct_search a').on('tap', function () {
         var key = $.trim($input.val());
-        if(!key){
+        if (!key) {
             mui.toast('请输入关键字');
             return false;
         }
         getSearchData({
             proName: key,
-            page:1,
+            page: 1,
             pageSize: 4
-        },function (data) {
-            $('.ct_product').html(template('list',data));
+        }, function (data) {
+            $('.ct_product').html(template('list', data));
         });
     });
 
     /*4.搜索结果排序功能*/
-    $('.ct_order     a').on('tap',function () {
+    $('.ct_order     a').on('tap', function () {
         /*当前点击的A*/
         var $this = $(this);
         /*如果之前没有选择*/
-       if(!$this.hasClass('now')){
-           /*选中，并且取消其他的选中,箭头默认朝下*/
+        if (!$this.hasClass('now')) {
+            /*选中，并且取消其他的选中,箭头默认朝下*/
             $this.addClass('now').siblings().removeClass('now').find('span').removeClass('fa-angle-up').addClass('fa-angle-down');
-       }else{
-           /*当前已经有被选择的分类，则改变箭头的方向*/
-           if($this.find('span').hasClass('fa-angle-down')){
-               $this.find('span').removeClass('fa-angle-down').addClass('fa-angle-up');
-           }else{
-               $this.find('span').removeClass('fa-angle-up').addClass('fa-angle-down');
-           }
-       }
+        } else {
+            /*当前已经有被选择的分类，则改变箭头的方向*/
+            if ($this.find('span').hasClass('fa-angle-down')) {
+                $this.find('span').removeClass('fa-angle-down').addClass('fa-angle-up');
+            } else {
+                $this.find('span').removeClass('fa-angle-up').addClass('fa-angle-down');
+            }
+        }
+        /*获取当前点击的功能参数*/
+        var order = $this.attr('data-order');
+        var orderVal = $this.find('span').hasClass('fa-angle-up') ? 1 : 2;
+        var key = $.trim($input.val());
+        if(!key){
+            mui.toast('请输入关键字');
+            return false;
+        }
+        var params = {
+            proName: key,
+            page: 1,
+            pageSize: 4
+        };
+        params[order] = orderVal;
+        getSearchData(params, function (data) {
+            $('.ct_product').html(template('list', data));
+        });
     });
+
 
 });
 
